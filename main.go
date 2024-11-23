6e8fb27b997322578e046f5fca3c6156ae5513ed
tqtwin <thangtran081003@gmail.com>
2024-11-23 10:05:05 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Tạo router Gin
+	router := gin.Default()
+
+	// Định nghĩa route GET gốc "/"
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, Gin!",
+		})
+	})
+
+	// Chạy server tại cổng 8080
+	router.Run(":4000")
+}
